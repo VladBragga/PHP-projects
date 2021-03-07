5df349bd5f37d94a0ab53b2e2ffc9f2933280425
VladBragga <79112212+VladBragga@users.noreply.github.com>
2021-03-07 21:05:34 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Guitar ABC. </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Fav Icon -->
+	<link id="1" rel="icon" type="image/png" href="img/1.ico" />
+	<!-- Google Font Raleway -->
+	<link href='https://fonts.googleapis.com/css?family=Raleway:200,300,500,400,600,700,800' rel='stylesheet' type='text/css'>
+	<!-- Google Font Dancing Script -->
+	<link href='https://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<!-- Font Awesome CSS -->
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+	<!-- Owl Carousel CSS -->
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css" />
+	<!-- Animate CSS -->
+	<link rel="stylesheet" type="text/css" href="css/animate.min.css" />
+	<!-- simpleLens CSS -->
+	<link rel="stylesheet" type="text/css" href="css/jquery.simpleLens.css" />
+	<!-- Price Slider CSS -->
+	<link rel="stylesheet" type="text/css" href="css/jquery-price-slider.css" />
+	<!-- MeanMenu CSS -->
+	<link rel="stylesheet" type="text/css" href="css/meanmenu.min.css" />
+	<!-- Magnific Popup CSS -->
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css" />
+	<!-- Nivo Slider CSS -->
+	<link rel="stylesheet" type="text/css" href="css/nivo-slider.css" />
+	<!-- Stylesheet CSS -->
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<!-- Responsive Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/responsive.css" />
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+<div class="header-area"><!--Start Header Area-->
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 col-lg-3">
+				<div class="log-link">
+					<p><span class="lang2">Режим адміністратора</span></p>
+					<h3><u><a href="login.php">Вхід</u></a> </h4>
+				</div>
+			</div>
+			
+			<div class="col-sm-4 col-lg-6">
+				<div class="logo text-center">
+					<a href="index.php">
+						<!-- <img src="img/header/logo.png" alt="" /> -->
+						<h1><span class="select">Guitar ABC.</span></h1>
+						<span class="lang2"> бібліотека гітарного світу</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div><!--End Header Area-->
+<div class="menu-area"><!--Start Main Menu Area-->
+	<div class="container">
+		<div class="row">
+			<div class="clo-md-12">
+				<div class="main-menu hidden-sm hidden-xs">
+					<nav>
+						<ul>
+							<li><a href="index.php" class="active"><span class="lang2">Головна</span></a>
+							</li>
+							<li><a ><span class="lang2">Гітари</span></a>
+								<ul class="sub-menu">
+									<li><a href="gitara_1.php">Акустичні гітари</a></li>
+									<li><a href="gitara_2.php">Електрогітари</a></li>
+									<li><a href="gitara_3.php">Класичні гітари</a></li>
+									<li><a href="gitara_4.php">Бас гітари</a></li>
+								</ul>
+							</li>
+							<li><a><span class="lang2">Комбопідсилювачі</span></a>
+								<ul class="sub-menu">
+									<li><a href="combik_1.php">Для акустичної гітари</a></li>
+									<li><a href="combik_2.php">Для електрогітари</a></li>
+									<li><a href="combik_3.php">Для басу</a></li>
+								</ul>
+							</li>
+							
+							<li><a href="pedal.php"><span class="lang2">Педалі ефектів </span></a>
+							
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+</div><!--End Main Menu Area-->
+<!-- HOME SLIDER -->
+<div class="slider-wrap home-1-slider">
+		<div id="mainSlider" class="nivoSlider slider-image">
+		<img src="img/главная1.jpg" width="100%" hight="80%" alt="main slider" title="#htmlcaption"/>
+	</div>
+	<div id="htmlcaption" class="nivo-html-caption slider-caption">						
+		<div class="slider-progress"></div>							
+			<div class="slide1-text slide-text">
+			<div>
+				<div class="cap-title wow slideInRight" data-wow-duration=".9s" data-wow-delay="0s">
+					<h1>Гітари та гітарне обладнання</h1>
+				</div>
+				<div class="cap-dec wow slideInRight" class="cap-title" data-wow-duration="1.3s" data-wow-delay="0s">
+					<h2>На будь-який смак</h2>
+				</div>	
+				<div class="cap-title1 wow slideInRight" data-wow-duration="1.5s" data-wow-delay="0s">
+					<h1>Знайди своє</h1>
+					
+				</div>	
+			</div>	
+		</div>						
+	</div>
+</div>
+                                     <!-- HOME SLIDER -->
+<div class="featured-product section fix"><!--start Featured Product Area-->
+	<div class="container">
+		<div class="row">
+			<div class="section-title">
+				<h2>Розділи асортимунту сайту</h2>
+				<div class="underline"></div>
+			</div>
+
+			<div class="col-sm-11"  >
+				<!-- Featured slider Area Start -->
+				<div class="col-sm-1" >
+				</div>
+				<div class="col-sm-3"  style="margin-left: 50px">
+									   <!-- Single Product Start -->		  
+					<div class="product-item fix">
+						
+							<!-- Product image -->
+							<a class="pro-image fix" href="gitara_1.php"><img  src="img/превью гитары.jpg" width="260px" height="190px"/></a>
+							<!-- Product action Btn -->
+					
+						<div class="pro-name-price-ratting">
+							<!-- Product Name -->
+							<div class="pro-name">
+								<h4 style="margin-left: 80px"><a href="gitara_1.php">Гітари</a></h4>
+							</div>
+						</div>
+					</div><!-- Single Product End -->
+				</div>
+				<div class="col-sm-3" style="margin-left: 40px">                                      <!-- Single Product Start -->
+					
+				<div class="product-item fix">
+						
+							<!-- Product image -->
+							<a class="pro-image fix" href="combik_1.php"><img  src="img/превью комбики.jpg" width="260px" height="190px"/></a>
+							<!-- Product action Btn -->
+					
+						<div class="pro-name-price-ratting">
+							<!-- Product Name -->
+							<div class="pro-name">
+								<h4 style="margin-left: 50px"><a href="combik_1.php">Комбопідсилювачі</a></h4>
+							</div>
+						</div>
+					</div><!-- Single Product End -->
+				</div>    
+				<div class="col-sm-3" style="margin-left: 40px">                                      <!-- Single Product Start -->
+					
+					<div class="product-item fix">
+							
+								<!-- Product image -->
+								<a class="pro-image fix" href="pedal.php"><img  src="img/ped.jpg" width="260px" height="190px"/></a>
+								<!-- Product action Btn -->
+						
+							<div class="pro-name-price-ratting">
+								<!-- Product Name -->
+								<div class="pro-name">
+								<h4 style="margin-left: 45px"><a href="pedal.php">Педалі ефектів</a></h4>
+								</div>
+							</div>
+						</div><!-- Single Product End -->
+					</div>  
+			</div>
+		</div>
+	</div>
+<!--End Featured Product Area-->
+
+
+<div class="testimonial-area fix"><!--Start Testimonial Area-->
+	<div class="overlay section">
+		<div class="container">
+			<div class="row">
+					
+						<div class="testimonial-item">
+							<div class="image fix">
+								<img src="img/1.ico" alt="" />
+							</div>
+							<div class="content fix">
+							<p>Ласкаво просимо на сайт для естрадних, класичних гітаристів або тих, хто хоче стати одним з них. Тут ви можете знайти потрібну вам модель цього чудового 6-струнного інструмента. Також є перелік 
+									комбопідсилювачів та кабінетів. І також різноманіття педалів ефектів та процесорів, котрі надають нового звучання інструменту.</p>
+								<h3>Guitar ABC.</h3>
+							</div>
+						</div>
+				
+			
+			</div>
+		</div>
+	</div>
+</div><!--End Testimonial Area-->
+
+<!-- jQuery 2.1.4 -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- Bootstrap JS -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- Owl Carousel JS -->
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<!--countTo JS -->
+<script type="text/javascript" src="js/jquery.countTo.js"></script>
+<!-- mixitup JS -->
+<script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+<!-- magnific popup JS -->
+<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+<!-- Appear JS -->
+<script type="text/javascript" src="js/jquery.appear.js"></script>
+<!-- MeanMenu JS -->
+<script type="text/javascript" src="js/jquery.meanmenu.min.js"></script>
+<!-- Nivo Slider JS -->
+<script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+<!-- Scrollup JS -->
+<script type="text/javascript" src="js/jquery.scrollup.min.js"></script>
+<!-- simpleLens JS -->
+<script type="text/javascript" src="js/jquery.simpleLens.min.js"></script>
+<!-- Price Slider JS -->
+<script type="text/javascript" src="js/jquery-price-slider.js"></script>
+<!-- WOW JS -->
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script>
+	new WOW().init();
+</script>	
+<!-- Main JS -->
+<script type="text/javascript" src="js/main.js"></script>
+
+</body>
+
+</html>
